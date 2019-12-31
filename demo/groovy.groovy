a72c0fa66abf4f1e411b8362eacbdb510e745074
Simcha Wood <31593126+SimchaWood@users.noreply.github.com>
2019-12-31 15:51:53 -0500

    Data Types & Objective-C stuff

--- demo/groovy.groovy
+++ demo/groovy.groovy
@@ -1,5 +1,6 @@
 #!/usr/bin/env groovy
 import java.lang.BigDecimal
+package foo.bar
 
 println "Hello from the shebang line"
 
@@ -10,6 +11,10 @@ float f = 3.14;
 BigDecimal bd = 3.14;
 
 def pattern = ~/^(?:(start))(?<!foo)(?<=bar)MUSKET_(?<cfgName>[A-Z0-9_]+)(._fibble\d*){0,1}(?=foo)(?!bar)$/
+
+interface Greeter {                                         
+    void greet(String name)                                 
+}
 
 /**
  * A Class description
@@ -33,4 +38,17 @@ static class Person {
     public static void foobar() {}
 }
 
-package foo.bar
+def person = [name: 'Guillaume', age: 36]
+assert "$person.name is $person.age years old" == 'Guillaume is 36 years old'
+
+def sum = "The sum of 2 and 3 equals ${2 + 3}"
+assert sum.toString() == 'The sum of 2 and 3 equals 5'
+
+int xInt = 0b10101111
+BigInteger xBigInteger = 0b111100100001
+
+def startingAndEndingWithANewline = '''
+line one
+line two
+line three
+'''
