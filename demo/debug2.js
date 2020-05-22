8a1c56b904258cb4ef51cf5463163bec39717c18
Simcha Wood <truthfulthomas@gmail.com>
2020-05-22 15:46:10 +0000

    Init Jupyter support

--- demo/debug2.js
+++ demo/debug2.js
@@ -1,9 +1,11 @@
 /* eslint-disable strict */
+let foo = "foobar";
 
 function stringThing(str) {
   return str.toUpperCase();
 }
 
 // Todo: Stuff
+foo = stringThing(foo);
 
 module.exports = stringThing;
